311224f12d0adf36c0d167a8b9d1c8434dfa94fb
Jeonsooyoung <jsooyoung05@gmail.com>
2026-06-23 20:31:32 +0900

    feat: app.py에 diary 실행 기능 추가

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from backend.photo.photo_routes import PhotoBlueprint
+from backend.diary.diary_routes import diary_bp
 
 
 app = Flask(__name__)
 
 app.register_blueprint(PhotoBlueprint)
+app.register_blueprint(diary_bp)
 
 
 if __name__ == "__main__":
