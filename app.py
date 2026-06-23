c4441579e95e50b50779f9278e73d7d119506027
Jeonsooyoung <jsooyoung05@gmail.com>
2026-06-23 20:32:50 +0900

    feat: app.py에 에러 관련 기능 추가

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from backend.error_handler import register_error_handlers
 from backend.photo.photo_routes import PhotoBlueprint
 from backend.diary.diary_routes import diary_bp
 
 
 app = Flask(__name__)
+
+register_error_handlers(app)
 
 app.register_blueprint(PhotoBlueprint)
 app.register_blueprint(diary_bp)
